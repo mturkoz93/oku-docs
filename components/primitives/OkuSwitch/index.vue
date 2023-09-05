2a5c7811ae6750e39f76654fb3b264d37b1fe391
emirulucay <eux202@gmail.com>
2023-09-05 13:43:18 +0300

    feat: switch

--- components/primitives/OkuSwitch/index.vue
+++ components/primitives/OkuSwitch/index.vue
@@ -11,7 +11,7 @@ import { OkuSwitch, OkuSwitchThumb } from '@oku-ui/switch'
         </label>
         <OkuSwitch
           id="airplane-mode"
-          class="w-[42px] h-[25px] bg-blackA-950 rounded-full relative shadow-[0_2px_10px] dark:shadow-blackA-700 focus:shadow-[0_0_0_2px] focus:shadow-oku-600 data-[state=checked]:bg-black outline-none cursor-default"
+          class="w-[42px] h-[25px] bg-grayA-300 dark:bg-grayA-500 rounded-full relative shadow-[0_2px_10px] shadow-grayA-200 dark:shadow-grayA-900 focus:shadow-[0_0_0_2px] focus:shadow-grayA-900 dark:focus:shadow-grayA-300 data-[state=checked]:bg-oku-500 dark:data-[state=checked]:bg-oku-500 outline-none cursor-default"
         >
           <OkuSwitchThumb class="block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA-600 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
         </OkuSwitch>
