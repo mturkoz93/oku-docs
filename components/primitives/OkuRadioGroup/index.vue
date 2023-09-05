255ce4cc360ce29f0f1c2b5b57e46dce7729fef1
emirulucay <eux202@gmail.com>
2023-09-05 13:30:31 +0300

    feat: radio group

--- components/primitives/OkuRadioGroup/index.vue
+++ components/primitives/OkuRadioGroup/index.vue
@@ -13,36 +13,36 @@ import { OkuRadioGroup, OkuRadioGroupIndicator, OkuRadioGroupItem } from '@oku-u
         <div class="flex items-center">
           <OkuRadioGroupItem
             id="r1"
-            class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
+            class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-grayA-300 dark:shadow-grayA-800 hover:bg-oku-100 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
             value="default"
           >
             <OkuRadioGroupIndicator class="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-oku-500" />
           </OkuRadioGroupItem>
-          <label class="text-white text-[15px] leading-none pl-[15px]" htmlFor="r1">
+          <label class="text-codGray-700 dark:text-white text-[15px] leading-none pl-[15px]" htmlFor="r1">
             Default
           </label>
         </div>
         <div class="flex items-center">
           <OkuRadioGroupItem
             id="r2"
-            class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
+            class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-grayA-300 dark:shadow-grayA-800 hover:bg-oku-100 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
             value="comfortable"
           >
             <OkuRadioGroupIndicator class="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-oku-500" />
           </OkuRadioGroupItem>
-          <label class="text-white text-[15px] leading-none pl-[15px]" htmlFor="r2">
+          <label class="text-codGray-700 dark:text-white text-[15px] leading-none pl-[15px]" htmlFor="r2">
             Comfortable
           </label>
         </div>
         <div class="flex items-center">
           <OkuRadioGroupItem
             id="r3"
-            class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
+            class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-grayA-300 dark:shadow-grayA-800 hover:bg-oku-100 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default"
             value="compact"
           >
             <OkuRadioGroupIndicator class="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-oku-500" />
           </OkuRadioGroupItem>
-          <label class="text-white text-[15px] leading-none pl-[15px]" htmlFor="r3">
+          <label class="text-codGray-700 dark:text-white text-[15px] leading-none pl-[15px]" htmlFor="r3">
             Compact
           </label>
         </div>
